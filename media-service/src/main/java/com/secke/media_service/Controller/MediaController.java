8080e842d1ff5d922464a38e71554c3979f79092
Djibril SOW <96235334+jeebrail@users.noreply.github.com>
2024-11-07 13:02:58 +0000

    exeption for size and type

--- media-service/src/main/java/com/secke/media_service/Controller/MediaController.java
+++ media-service/src/main/java/com/secke/media_service/Controller/MediaController.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 import com.secke.media_service.Model.Media;
 import com.secke.media_service.Repository.MediaRepository;
@@ -28,10 +32,30 @@ public class MediaController {
     @PostMapping("/upload")
     public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file,
                                          @RequestParam("productId") String productId) {
+        
+        if (file.getSize() > 2 * 1024 * 1024) { // 2MB limit
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "File size exceeds the 2MB limit");
+        }
+
+        // Validate file typee if not img
+        try {
+            String contentType = file.getContentType();
+            if (contentType == null || !contentType.startsWith("image/")) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid file type. Only image files are allowed.");
+            }
+            
+            // Optionally, you can further check the file with ImageIO
+            if (ImageIO.read(file.getInputStream()) == null) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "File is not a valid image.");
+            }
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error reading the image file.");
+        }
+
         try {
             Media media = mediaService.uploadImage(file, productId);
 
-            // Message de succès
+            // successsss
             Map<String, String> response = new HashMap<>();
             response.put("message", "Upload successful");
             response.put("mediaId", media.getId());
